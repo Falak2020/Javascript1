3f62270af979ad95713d02539699ac9353c0356e
Falak <felekmurad@gmail.com>
2020-12-22 18:36:06 +0100

    first step

--- uppgift-1/js/script.js
+++ uppgift-1/js/script.js
@@ -17,19 +17,21 @@ const edit = document.querySelector('#editBtn')
 const listUsers = () => {
     output.innerHTML = ''
     usersList.forEach(user => {
-        output.innerHTML+=`<div  id="${user.Id}"class="bg-white border rounded p-2 d-flex justify-content-between align-items-center mt-1"><div><div class="displayName">${user.FirstName} ${user.LastName}</div><div class="emailStyle">${user.Email} </div></div><button class="btn btn-danger px-3">Delete</button><button  class="btn btn-success px-3">Edit</button></div>`
+        output.innerHTML+=`<div  id="${user.Id}"class="bg-white border rounded p-2 d-flex justify-content-between align-items-center mt-1"><div><div class="displayName">${user.FirstName} ${user.LastName}</div><div class="emailStyle">${user.Email} </div></div><div><button class="btn btn-danger px-3">Delete</button><button  class="btn btn-success px-3 ms-4">Edit</button></div></div>`
     })
 }
 function ValidateEmail(string){
     if(string.includes('ä')||string.includes('å') ||string.includes('ö'))
-    return false
-    else
     return true
+    else
+    return false
 }
+
 function Validateform(){
     if (firstName.value !== '' && lasName.value !== '' && id.value !== '' && email.value !== '') {
-        console.log( email.value)
-        if (firstName.value.length >= 3 && lasName.value.length >= 3 && ValidateEmail(email.value)) {
+       console.log(email.value)
+       console.log(ValidateEmail(email.value))
+        if (firstName.value.length >= 3 && lasName.value.length >= 3 && !ValidateEmail(email.value)) {
             firstName.classList.remove('is-invalid')
             lastName.classList.remove('is-invalid')
             id.classList.remove('is-invalid')
@@ -53,8 +55,6 @@ function Validateform(){
                     }
                 } 
             }
-           
-           
             listUsers()
             firstName.value = ''
             lastName.value = ''
@@ -72,7 +72,11 @@ function Validateform(){
             lastName.classList.add('is-invalid')
             lastNameError.innerHTML = '<div class="error">The last name must be more than 2 char</div>'
             lastName.value = ''
+        }else{
+            emailError.innerHTML='<div class="error">The email should not contain ä,å,ö</div>'
+            email.value=''
         }
+
     }
     else if (firstName.value === '') {
         firstName.classList.add('is-invalid')
@@ -105,8 +109,10 @@ submitBtn.addEventListener('click', (e) => {
 
 output.addEventListener('click',(e)=>{
    const button=e.target
+   console.log(button.textContent)
    if(button.textContent==='Delete'){
-    usersList = usersList.filter(user => user.Id !== e.target.parentNode.id)
+    console.log(e.target.parentNode.parentNode.id)
+    usersList = usersList.filter(user => user.Id !== e.target.parentNode.parentNode.id)
     listUsers()
    }
    else{
